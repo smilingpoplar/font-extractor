90d209a80cf795ab51346232f20d91f1dd688ccb
smilingpoplar <smilingpoplar@gmail.com>
2012-04-16 18:03:55 +0800

    add font-extractor

---
+++ font-extractor.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# author: YangLe 201204
+
+import sys
+import os
+
+def run(cmd):
+    print cmd
+    os.system(cmd)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print 'usage: ./font-extractor.py [stringsfile.strings] [inputfile.ttf] [outputfile.ttf]]'
+        exit(-1)
+    stringsfile, inputfile, outputfile = sys.argv[1:]
+    suffix = '.ttf'
+    if outputfile.endswith(suffix):
+        fontName = outputfile[:-len(suffix)]
+    else:
+        fontName = outputfile
+        outputfile = outputfile + suffix
+
+    tmpDir = '/var/tmp/font-extractor/'
+    os.system('mkdir -p %s' % tmpDir)
+
+    # generate charsfile
+    tmpCharsfile = tmpDir + 'tmp.charsfile'
+    cmdExtractStrings = '''
+    ./strings-extractor.py %(stringsfile)s %(charsfile)s
+    ''' % {
+        'stringsfile' : stringsfile,
+        'charsfile' : tmpCharsfile
+        }
+    run(cmdExtractStrings)
+
+    # generate a subset of font
+    tmpTtf = tmpDir + 'tmp.ttf'
+    cmdSubset = '''
+    cd font-optimizer
+    ./subset.pl --charsfile=%(charsfile)s %(inputfile)s %(outputfile)s
+    ''' % {
+        'charsfile' : tmpCharsfile,
+        'inputfile' : inputfile,
+        'outputfile': tmpTtf
+        }
+    run(cmdSubset)
+    
+    cmdModifyNames = '''
+    cd font-optimizer
+    ./modify-names.pl --set family %(fontName)s %(inputfile)s %(outputfile)s
+     mv %(outputfile)s ..
+    ''' % {
+        'fontName' : fontName,
+        'inputfile' : tmpTtf,
+        'outputfile' : outputfile
+        }
+    run(cmdModifyNames)
+    
